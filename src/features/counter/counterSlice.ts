0e1191762b699a216296300942b43b3529c9fdfe
ragg09 <ragunayon@gmail.com>
2023-09-14 15:14:04 +0800

    RTK configuration

---
+++ src/features/counter/counterSlice.ts
@@ -0,0 +1,34 @@
+// This file is included to RTK configuration
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+
+export interface CounterState {
+  value: number;
+}
+
+const initialState: CounterState = {
+  value: 0,
+};
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    reset: (state) => {
+      state.value = 0;
+    },
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value += action.payload;
+    },
+  },
+});
+
+export const { increment, decrement, reset, incrementByAmount } = counterSlice.actions;
+
+export default counterSlice.reducer;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
+import { store } from '@/redux/store';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
 
 const App: React.FunctionComponent<AppProps> = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 };
 
 export default App;
