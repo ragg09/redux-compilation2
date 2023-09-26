f78b24eb5c5eb928ba52838a5840f059d9c2ee05
ragg09 <ragunayon@gmail.com>
2023-09-26 15:41:32 +0800

    Optimized Image

--- src/shared/components/Card/FlippingCard.tsx
+++ src/shared/components/Card/FlippingCard.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
 import { countFlipMove, flipCard } from '@/redux/features/flipCard/flipCardSlice';
 import { useAppDispatch } from '@/redux/hook';
 import { type FlipCardData } from '@/shared/utils';
@@ -23,7 +22,7 @@ const FlippingCard: React.FC<FlipCardProps> = ({ className, card }: FlipCardProp
 
   return (
     <div
-      className="group max-w-[250px] max-h-[180px] h-80 w-49 [perspective:1000px] overflow-hidden"
+      className="group max-w-[250px] max-h-[180px] h-80 w-49 [perspective:1000px] overflow-hidden rounded-xl "
       onClick={() => {
         handleCardClick(card.tempID);
       }}
@@ -34,27 +33,24 @@ const FlippingCard: React.FC<FlipCardProps> = ({ className, card }: FlipCardProp
         }`}
       >
         <div className="absolute inset-0">
-          <img
-            className="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40"
-            src="/flip-card/backcard.jpg"
-            alt=""
+          <Image
+            src={'/flip-card/backcard.jpg'}
+            alt={'bg'}
+            width={250}
+            height={180}
+            priority={true}
+            style={{ width: '100%', height: '100%' }}
           />
         </div>
 
         <div className="absolute h-full w-full rounded-xl bg-black/80  text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]">
           <Image
             src={`/flip-card/${card.image}`}
+            alt={`${card.alt} | ${card.tempID}`}
             width={250}
             height={180}
-            alt={`${card.alt} | ${card.tempID}`}
+            style={{ width: '100%', height: '100%' }}
           />
-
-          {/* <img
-            className="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40"
-            src={`/flip-card/${card.image}`}
-            // src="https://images.unsplash.com/photo-1562583489-bf23ec64651d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')"
-            alt={`${card.alt} | ${card.tempID}`}
-          /> */}
         </div>
       </div>
     </div>
